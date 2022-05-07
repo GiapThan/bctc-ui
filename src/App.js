c879fa377f6d454f8bfd56704f08c32ee8ea5cb5
GiapThan <88590392+GiapThan@users.noreply.github.com>
2022-05-07 21:04:07 +0700

    nodemailer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import emailjs from 'emailjs-com'; 
 import { useState } from 'react'
+import nodemailer from 'nodemailer'
 
 function App() {
 const [name, setName] = useState('')
@@ -17,9 +18,30 @@ const [subject, setSubject] = useState('')
      .catch()
     
 } 
+function Sub {
+   const transporter = nodemailer.createTransport({ 
+           service: "Gmail",
+           port: 465,
+           auth: { 
+                user: "cucuxom@gmail.com", 
+                pass: "cucuxom123123",
+            },
+         })
+   const option = {
+              from: "giapthan0604@gmail.com",
+              to: "cucuxom@gmail.com",
+              subject: "demo nef",
+              text: "hello guy",
+         }
+   transporter.sendMail(option, (e,i) => {
+      alert("err  ", e)
+      alert("info  ", i)
+   })
+  transporter.close()
+}
   return ( 
        <div className="App"> 
-           <form onSubmit={e=>Submit(e)}> 
+           <form onSubmit={Sub}> 
 <input name='name' placeholder='Your Name...' value={name} onChange={e=>setName(e.target.value)} /> 
 <input name='email' placeholder='Your Email...' value={email} onChange={e=>setEmail(e.target.value)} /> 
 <input name='subject' placeholder='Your Subject...' value={subject} onChange={e=>setSubject(e.target.value)} /> 
